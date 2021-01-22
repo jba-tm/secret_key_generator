0e86cc34aa9fb4e80941aea1672fce3a55c0a516
Batyr Jumageldiyew <jumageldiyewb@list.ru>
2021-01-22 14:35:39 +0500

    Fix import

--- secret_key_generate.py
+++ secret_key_generate.py
@@ -1,1 +1,3 @@
-python -c 'import random; print("".join(random.SystemRandom().choice("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)") for _ in range(50)))'
+import random
+
+print("".join(random.SystemRandom().choice("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)") for _ in range(50)))
